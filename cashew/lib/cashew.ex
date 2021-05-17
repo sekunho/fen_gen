c806230bae297c1cf29df841dd4589b633e1f6a0
Sek Un <sekun@hey.com>
2021-05-18 02:50:17 +0800

    Add image dump to bin file

--- cashew/lib/cashew.ex
+++ cashew/lib/cashew.ex
@@ -1,18 +1,21 @@
 defmodule Cashew do
-  @moduledoc """
-  Documentation for `Cashew`.
-  """
+  alias Pixels
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cashew.hello()
-      :world
-
+  Dump images data into a bin file.
   """
-  def hello do
-    :world
+  def dump_images!(images, name \\ "data.bin") when is_list(images) and is_binary(name) do
+    [%{height: rows, width: cols} | _] = images
+    count = length(images)
+
+    img_bins =
+      Enum.map(images, fn img ->
+        img.data
+      end)
+      |> :binary.list_to_bin()
+
+    bin = <<count::32, rows::32, cols::32, img_bins::binary>>
+
+    File.write(name, bin)
   end
 end
