e790dbf74158ca731774332c5dd8f0800f852b4b
Sek Un <sekun@hey.com>
2021-05-27 22:15:33 +0800

    Update

--- lib/fen_gen_web/live/page_live.ex
+++ lib/fen_gen_web/live/page_live.ex
@@ -1,8 +1,30 @@
 defmodule FenGenWeb.PageLive do
   use Surface.LiveView
+  alias FenGenWeb.Router.Helpers, as: Routes
+
+  data uploaded_files, :list, default: []
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, allow_upload(socket, :board, accept: ~w(.jpg .jpeg), max_entries: 4)}
+  end
+
+  @impl true
+  def handle_event("validate", _params, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("save", _params, socket) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :board, fn %{path: path}, _entry ->
+        dest = Path.join([:code.priv_dir(:fen_gen), "static", "uploads", Path.basename(path)])
+        IO.inspect(dest)
+        File.mkdir(dest) |> IO.inspect()
+        File.cp!(path, dest)
+        Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
+      end)
+
+    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
   end
 end
--- shell.nix
+++ shell.nix
@@ -12,6 +12,7 @@ mkShell {
   LOCALE_ARCHIVE_2_27 = "${glibcLocales}/lib/locale/locale-archive";
 
   buildInputs = with python38Packages; [
+    cacert
     nodejs
     elixir
     erlang
