84b5059fc05be6dfd90c6f78aa1c221eeee32c33
Sek Un <sekun@hey.com>
2021-05-28 16:03:45 +0800

    Represent prediction in board state

--- lib/fen_gen_web/live/components/tile.ex
+++ lib/fen_gen_web/live/components/tile.ex
@@ -1,4 +1,8 @@
 defmodule FenGenWeb.Components.Tile do
   use Surface.Component
 
+  @state ["*", "r", "n", "b", "q", "k", "p", "R", "N", "B", "Q", "K", "P",]
+
+  prop current_state, :string, values: @state, default: "*"
+
 end
